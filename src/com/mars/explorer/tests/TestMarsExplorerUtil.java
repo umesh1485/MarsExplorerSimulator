0b507f7ad2df0dec1661867c31be72a536904594
umesh sonkar <umesh_sonkar@optum.com>
2019-07-16 16:36:26 +0530

    Created jar from maven

--- src/com/mars/explorer/tests/TestMarsExplorerUtil.java
+++ src/com/mars/explorer/tests/TestMarsExplorerUtil.java
@@ -3,8 +3,8 @@ package com.mars.explorer.tests;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.gen5.api.Assertions;
+import org.junit.gen5.api.Test;
 
 import com.mars.explorer.data.MarsExplorerData;
 import com.mars.explorer.util.MarsExplorerUtil;
@@ -24,14 +24,14 @@ public class TestMarsExplorerUtil {
 		path.add(1+","+2);
 		path.add(1+","+3);
 		path.add(0+","+3);
-		Assert.assertTrue(MarsExplorerData.path.equals(path));
+		Assertions.assertTrue(MarsExplorerData.path.equals(path));
 		
 		List<String> exploredUnits = new ArrayList<>();
 		exploredUnits.add(0+","+0);
 		exploredUnits.add(0+","+3);
 		
-		Assert.assertTrue(MarsExplorerData.exploredUnits.equals(exploredUnits));
-		Assert.assertTrue(MarsExplorerData.currentPosition.equals(0+","+3));
+		Assertions.assertTrue(MarsExplorerData.exploredUnits.equals(exploredUnits));
+		Assertions.assertTrue(MarsExplorerData.currentPosition.equals(0+","+3));
 		
 		
 
